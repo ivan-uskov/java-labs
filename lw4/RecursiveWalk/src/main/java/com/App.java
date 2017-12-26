324d60f9dc275c7a527a1ff111b23ae160000992
Ivan Uskov <ivan.uskov@cpslabs.net>
2017-12-26 08:53:46 +0300

    comple main function and add test directory

--- lw4/RecursiveWalk/src/main/java/com/App.java
+++ lw4/RecursiveWalk/src/main/java/com/App.java
@@ -1,9 +1,12 @@
 package com;
 
-import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class App {
@@ -17,18 +20,29 @@ public class App {
             Stream<String> stream = Files.lines(Paths.get(args[0]));
             FileSystemWalker walker = new FileSystemWalker();
             stream.forEach(walker::visit);
-            walker.getVisitedFiles().forEach(App::processFile);
+
+            PrintStream out = new PrintStream(new FileOutputStream(args[1]));
+            walker.getVisitedFiles().forEach(getFileProcessor(out));
+            walker.getInvalidPaths().forEach(getInvalidPathProcessor(out));
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
     }
 
-    private static void processFile(Path path) {
-        FNVHasher hasher = new FNVHasher();
-        FileWalker walker = new FileWalker(hasher::add);
-        if (walker.walk(path)) {
-            System.out.println(hasher.getHash() + " " + path.toString());
-        }
+    private static Consumer<String> getInvalidPathProcessor(PrintStream out) {
+        return (String path) -> {
+            out.printf("invalid path - %s\n", path);
+        };
+    }
+
+    private static Consumer<Path> getFileProcessor(PrintStream out) {
+        return (Path path) -> {
+            FNVHasher hasher = new FNVHasher();
+            FileWalker walker = new FileWalker(hasher::add);
+            if (walker.walk(path)) {
+                out.printf("%h - %s\n", hasher.getHash(), path.toString());
+            }
+        };
     }
 }
