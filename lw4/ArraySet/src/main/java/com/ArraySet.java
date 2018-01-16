3eb37ff4326d657bd70f904660c62e0c9563275b
Ivan Uskov <ivan.uskov@cpslabs.net>
2018-01-16 08:13:45 +0300

    Добавил реализацию ArraySet

--- lw4/ArraySet/src/main/java/com/ArraySet.java
+++ lw4/ArraySet/src/main/java/com/ArraySet.java
@@ -1,90 +1,196 @@
 package com;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class ArraySet<E> extends ArrayList<E> implements NavigableSet<E> {
+class ArraySet<E extends Comparable<E>> extends AbstractSet<E> implements NavigableSet<E> {
+    private final List<E> data;
+
+    ArraySet(E[] items) {
+        data = Arrays.stream(items).filter(Objects::nonNull).distinct().sorted().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private ArraySet(List<E> d) {
+        data = d;
+    }
+
+    private class ArraySetIterator<T> implements Iterator<T> {
+        private final boolean isAscOrder;
+        private final List<T> list;
+        private int position;
+
+        ArraySetIterator(List<T> l, boolean asc) {
+            list = l;
+            isAscOrder = asc;
+            position = isAscOrder ? 0 : list.size() - 1;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return isAscOrder ? position < list.size() : position >= 0;
+        }
+
+        @Override
+        public T next() {
+            return list.get(isAscOrder ? position++ : position--);
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
     @Override
     public E lower(E e) {
-        return null;
+        int index = Collections.binarySearch(data, e);
+        if (index < 0) {
+            index = Math.abs(index + 1);
+        }
+
+        return indexExists(index - 1) ? data.get(index - 1) : null;
     }
 
     @Override
     public E floor(E e) {
-        return null;
+        int index = Collections.binarySearch(data, e);
+        if (index >= 0) {
+            return indexExists(index) ? data.get(index) : null;
+        }
+
+        index = Math.abs(index + 1);
+        return indexExists(index - 1) ? data.get(index - 1) : null;
     }
 
     @Override
     public E ceiling(E e) {
-        return null;
+        int index = Collections.binarySearch(data, e);
+        if (index < 0) {
+            index = Math.abs(index + 1);
+        }
+
+        return indexExists(index) ? data.get(index) : null;
     }
 
     @Override
     public E higher(E e) {
-        return null;
+        int index = Collections.binarySearch(data, e);
+        if (index >= 0) {
+            return indexExists(index + 1) ? data.get(index + 1) : null;
+        }
+
+        index = Math.abs(index + 1);
+        return indexExists(index) ? data.get(index) : null;
     }
 
     @Override
     public E pollFirst() {
-        return null;
+        throw new UnsupportedOperationException("poll first on immutable set");
     }
 
     @Override
     public E pollLast() {
-        return null;
+        throw new UnsupportedOperationException("poll last on immutable set");
     }
 
     @Override
     public NavigableSet<E> descendingSet() {
-        return null;
+        return new ArraySet<>(new ArrayList<E>(data){
+            @Override
+            public E get(int index) {
+                boolean exists = index >= 0 && index < size();
+                return data.get(exists ? size() - index - 1 : index);
+            }
+        });
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ArraySetIterator<>(data, true);
     }
 
     @Override
     public Iterator<E> descendingIterator() {
-        return null;
+        return new ArraySetIterator<>(data, false);
     }
 
     @Override
     public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
-        return null;
+        return tailSet(fromElement, fromInclusive).headSet(toElement, toInclusive);
     }
 
     @Override
     public NavigableSet<E> headSet(E toElement, boolean inclusive) {
-        return null;
+        if (toElement == null) {
+            throw new NullPointerException("toElement is null");
+        } else if (isEmpty()) {
+            return this;
+        }
+
+        int index = Collections.binarySearch(data, toElement);
+        int last = (index >= 0 ? (inclusive ? index + 1 : index) : Math.abs(index + 1));
+
+        return new ArraySet<>(last <= 0 ? new ArrayList<>() : data.subList(0, last));
     }
 
     @Override
     public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
-        return null;
+        if (fromElement == null) {
+            throw new NullPointerException("fromElement is null");
+        } else if (isEmpty()) {
+            return this;
+        }
+
+        int index = Collections.binarySearch(data, fromElement);
+        int first = (index >= 0 ? (inclusive ? index : index + 1) : Math.abs(index + 1));
+
+        return new ArraySet<>(first >= size() ? new ArrayList<>() : data.subList(first, size()));
     }
 
     @Override
     public SortedSet<E> subSet(E fromElement, E toElement) {
-        return null;
+        return subSet(fromElement, true, toElement, false);
     }
 
     @Override
     public SortedSet<E> headSet(E toElement) {
-        return null;
+        return headSet(toElement, false);
     }
 
     @Override
     public SortedSet<E> tailSet(E fromElement) {
-        return null;
+        return tailSet(fromElement, true);
     }
 
     @Override
     public Comparator<? super E> comparator() {
-        return null;
+        return E::compareTo;
     }
 
     @Override
     public E first() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty");
+        }
+
+        return data.get(0);
     }
 
     @Override
     public E last() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty");
+        }
+
+        return data.get(size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    private boolean indexExists(int index) {
+        return index >= 0 && index < size();
     }
 }
--- lw4/ArraySet/src/test/java/com/ArraySetTest.java
+++ lw4/ArraySet/src/test/java/com/ArraySetTest.java
@@ -2,11 +2,172 @@ package com;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NavigableSet;
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraySetTest {
     @Test
-    void floorTest() {
+    void firstLastOfEmptySetThrowsException() {
+        assertThrows(NoSuchElementException.class, (new ArraySet<>(new Integer[]{}))::first);
+        assertThrows(NoSuchElementException.class, (new ArraySet<>(new Integer[]{}))::last);
+    }
 
+    @Test
+    void firstReturnsFirstElement() {
+        assertEquals(new Integer(1), new ArraySet<>(new Integer[]{1, 2, 3, 4}).first());
+    }
+
+    @Test
+    void lastReturnsLastElement() {
+        assertEquals(new Integer(4), new ArraySet<>(new Integer[]{1, 2, 3, 4}).last());
+    }
+
+    @Test
+    void sizeReturnsCollectionSize() {
+        assertEquals(4, new ArraySet<>(new Integer[]{1, 2, 3, 4}).size());
+    }
+
+    @Test
+    void iteratorTest() {
+        Iterator<Integer> i = new ArraySet<>(new Integer[]{1, 2, 3, 4}).iterator();
+        assertEquals(new Integer(1), i.next());
+        assertThrows(UnsupportedOperationException.class, i::remove);
+        assertEquals(new Integer(2), i.next());
+        assertEquals(new Integer(3), i.next());
+        assertEquals(new Integer(4), i.next());
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    void descendingIteratorTest() {
+        Iterator<Integer> i = new ArraySet<>(new Integer[]{1, 2, 3, 4}).descendingIterator();
+        assertEquals(new Integer(4), i.next());
+        assertThrows(UnsupportedOperationException.class, i::remove);
+        assertEquals(new Integer(3), i.next());
+        assertEquals(new Integer(2), i.next());
+        assertEquals(new Integer(1), i.next());
+        assertFalse(i.hasNext());
+    }
+
+    @Test
+    void lowerTest() {
+        assertEquals(new Integer(2), (new ArraySet<>(new Integer[]{2, 3, 4, 5})).lower(3));
+        assertEquals(new Integer(4), (new ArraySet<>(new Integer[]{1, 2, 3, 4})).lower(5));
+        assertEquals(new Integer(6), (new ArraySet<>(new Integer[]{2, 4, 6, 8})).lower(7));
+        assertEquals(null, (new ArraySet<>(new Integer[]{2, 4, 6, 8})).lower(2));
+    }
+
+    @Test
+    void floorTest() {
+        assertEquals(new Integer(3), (new ArraySet<>(new Integer[]{2, 3, 4, 5})).floor(3));
+        assertEquals(new Integer(4), (new ArraySet<>(new Integer[]{1, 2, 3, 4})).floor(5));
+        assertEquals(new Integer(6), (new ArraySet<>(new Integer[]{2, 4, 6, 8})).floor(7));
+        assertEquals(null, (new ArraySet<>(new Integer[]{2, 4, 6, 8})).floor(1));
+    }
+
+    @Test
+    void ceilingTest() {
+        assertEquals(new Integer(3), (new ArraySet<>(new Integer[]{2, 3, 4, 5})).ceiling(3));
+        assertEquals(new Integer(1), (new ArraySet<>(new Integer[]{1, 2, 3, 4})).ceiling(0));
+        assertEquals(new Integer(8), (new ArraySet<>(new Integer[]{2, 4, 6, 8})).ceiling(7));
+        assertEquals(null, (new ArraySet<>(new Integer[]{2, 4, 6, 8})).ceiling(9));
+    }
+
+    @Test
+    void higherTest() {
+        assertEquals(new Integer(4), (new ArraySet<>(new Integer[]{2, 3, 4, 5})).higher(3));
+        assertEquals(new Integer(1), (new ArraySet<>(new Integer[]{1, 2, 3, 4})).higher(0));
+        assertEquals(new Integer(8), (new ArraySet<>(new Integer[]{2, 4, 6, 8})).higher(7));
+        assertEquals(null, (new ArraySet<>(new Integer[]{2, 4, 6, 8})).higher(8));
+    }
+
+    @Test
+    void pollThrowsException() {
+        assertThrows(UnsupportedOperationException.class, (new ArraySet<>(new String[]{})::pollFirst));
+        assertThrows(UnsupportedOperationException.class, (new ArraySet<>(new String[]{})::pollLast));
+    }
+
+    @Test
+    void headSetTest() {
+        ArraySet<Integer> s = new ArraySet<>(new Integer[]{1, 2, 4, 6});
+        assertTrue(new ArraySet<>(new Integer[]{1, 2}).equals(s.headSet(3, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2}).equals(s.headSet(3, false)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4}).equals(s.headSet(4, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2}).equals(s.headSet(4, false)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.headSet(7, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.headSet(7, false)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.headSet(1, false)));
+        assertTrue(new ArraySet<>(new Integer[]{1}).equals(s.headSet(1, true)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.headSet(0, true)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.headSet(0, false)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(new ArraySet<>(new Integer[]{}).headSet(0)));
+
+        assertThrows(NullPointerException.class, () -> s.headSet(null));
+    }
+
+    @Test
+    void tailSetTest() {
+        ArraySet<Integer> s = new ArraySet<>(new Integer[]{1, 2, 4, 6});
+        assertTrue(new ArraySet<>(new Integer[]{4, 6}).equals(s.tailSet(3, true)));
+        assertTrue(new ArraySet<>(new Integer[]{4, 6}).equals(s.tailSet(3, false)));
+        assertTrue(new ArraySet<>(new Integer[]{4, 6}).equals(s.tailSet(4, true)));
+        assertTrue(new ArraySet<>(new Integer[]{6}).equals(s.tailSet(4, false)));
+        assertTrue(new ArraySet<>(new Integer[]{6}).equals(s.tailSet(6, true)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.tailSet(6, false)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.tailSet(7, true)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(s.tailSet(7, false)));
+        assertTrue(new ArraySet<>(new Integer[]{2, 4, 6}).equals(s.tailSet(1, false)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.tailSet(1, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.tailSet(0, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.tailSet(0, false)));
+        assertTrue(new ArraySet<>(new Integer[]{}).equals(new ArraySet<>(new Integer[]{}).tailSet(0)));
+
+        assertThrows(NullPointerException.class, () -> s.tailSet(null));
+    }
+
+    @Test
+    void subSetTest() {
+        ArraySet<Integer> s = new ArraySet<>(new Integer[]{1, 2, 4, 6});
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.subSet(1, true, 6, true)));
+        assertTrue(new ArraySet<>(new Integer[]{2, 4}).equals(s.subSet(1, false, 6, false)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4}).equals(s.subSet(1, true, 6, false)));
+        assertTrue(new ArraySet<>(new Integer[]{2, 4, 6}).equals(s.subSet(1, false, 6, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.subSet(0, true, 7, true)));
+        assertTrue(new ArraySet<>(new Integer[]{1, 2, 4, 6}).equals(s.subSet(0, false, 7, false)));
+
+        assertThrows(NullPointerException.class, () -> s.subSet(null, 1));
+        assertThrows(NullPointerException.class, () -> s.subSet(1, null));
+    }
+
+    @Test
+    void comparatorNotNull() {
+        ArraySet<Integer> s = new ArraySet<>(new Integer[]{1, 2, 4, 6});
+        assertNotNull(s.comparator());
+    }
+
+    @Test
+    void descendingSetTest() {
+        ArraySet<Integer> s = new ArraySet<>(new Integer[]{1, 2, 4, 6});
+        NavigableSet<Integer> ds1 = s.descendingSet();
+        Iterator<Integer> i1 = ds1.iterator();
+        assertEquals(new Integer(6), i1.next());
+        assertThrows(UnsupportedOperationException.class, i1::remove);
+        assertEquals(new Integer(4), i1.next());
+        assertEquals(new Integer(2), i1.next());
+        assertEquals(new Integer(1), i1.next());
+        assertFalse(i1.hasNext());
+
+        NavigableSet<Integer> ds2 = ds1.descendingSet();
+        Iterator<Integer> i2 = ds2.iterator();
+        assertEquals(new Integer(1), i2.next());
+        assertThrows(UnsupportedOperationException.class, i2::remove);
+        assertEquals(new Integer(2), i2.next());
+        assertEquals(new Integer(4), i2.next());
+        assertEquals(new Integer(6), i2.next());
+        assertFalse(i1.hasNext());
     }
 }
